dfa7b6c35a11d52eab625eba7d105ee10de62546
Kocsis Gergely <kocsisger@gmail.com>
2026-03-02 12:40:59 +0100

    ItemsActivity starts and goes back

--- app/src/main/java/com/example/shoppinglist/ItemsActivity.java
+++ app/src/main/java/com/example/shoppinglist/ItemsActivity.java
@@ -1,6 +1,7 @@
 package com.example.shoppinglist;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,9 @@ public class ItemsActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void handleItemAdd(View view) {
+        finish();
     }
 }
